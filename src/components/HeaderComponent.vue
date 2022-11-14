69726617a38feb5fd1a3c1eda64666b2e9e70b0f
Saimon397 <simonecarrabba@gmail.com>
2022-11-14 18:16:13 +0100

    Comm1

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav class="navbar sticky-top">
+      <div class="container-fluid">
+        <a class="navbar-brand fs-1 text-white" href="#nogo">
+          <img src="/img/imgbin_logo-breaking-bad-png.png" width="100" alt="Logo"
+            class="d-inline-block fe-1 align-text-top">
+          Breaking Bad Api
+        </a>
+        <div class="form-floating bg-dark col-2 pt-0">
+          <select class="form-select pt-0 pb-0 fs-4 bg-dark text-white">
+            <option selected>Select Category</option>
+            <option value="1">Alive</option>
+            <option value="2">Dead</option>
+            <option value="3">Presumed dead</option>
+          </select>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '../node_modules/bootstrap/scss/bootstrap.scss'
+import './assets/style/general.scss'
+
+createApp(App).mount('#app')
